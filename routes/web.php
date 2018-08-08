6bf8c214ce8d015cd3c789e1823cbd0237213a30
nitdevv <nitesh@excellencetechnologies.in>
2018-08-08 17:42:31 +0530

    api call using guzzlehttp client

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getData', function(){ 
+
+    $url = "https://bumbag-dev.myshopify.com/admin/customers.json";
+    $user = "0e3c07917f8dc3d8024c096d873405d5";
+    $secret = "5ba37853a655552b0391dc852b45139f";   
+
+    $client = new GuzzleHttp\Client();
+    $res = $client->get($url, ['auth' =>  [$user, $secret]]);
+
+    return $res->getBody();
+    
+});
